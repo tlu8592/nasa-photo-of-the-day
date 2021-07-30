d5bbb947b6caa30e3531c7807ef7be7440ee8a47
Tony Lu <TonyLu@Tonys-iMac.fios-router.home>
2021-07-30 02:11:07 -0400

    added styling using styled components

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import axios from 'axios';
 import DailyPhoto from './components/DailyPhoto';
-
+import styled from 'styled-components';
 
 function App() {
   const [photoData, setPhotoData] = useState({});
@@ -12,14 +12,56 @@ function App() {
       .then(response => setPhotoData(response.data))
       .catch(error => console.log(error));
     }, [])
+  
+  const StyledAppDiv = styled.div`
+    color: white;
+    text-align: center;
+    // border: 5px solid yellow;
+    padding-bottom: 3em;
+    display: flex;
+    flex-direction: column;
+  ` 
+  const FlexContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    align-content: space-evenly;
+  `;
+
+  const StyledH1 = styled.h1`
+    font-size: 3.2em;
+    // border: 5px solid green;
+    text-transform: uppercase;
+    // font-family: 'Trebuchet MS', san-serif;
+    font-family: 'Copperplate', fantasy;
+  `;
+
+  const StyledH2 = styled.h2`
+    font-size: 2rem;
+    font-family: 'Garamond', serif;
+    // border: 5px solid blue;
+    margin-top: 0.1rem;
+    text-transform: uppercase;
+  `;
+
+  const StyledP = styled.p`
+    font-size: 2.5em;
+    font-style: italic;
+    font-family: 'Brush Script MT', cursive;
+    // border: 5px solid pink;
+    margin-top: 0.1rem;
+
+  `;
 
   return (
-    <div className="App">
-      <h1>Nasa Photo of the Day</h1>
-      <h2>Welcome!</h2>
-      <p>Enjoy it! Learn it!</p>
+    <StyledAppDiv>
+      <FlexContainer>
+        <StyledH1>Nasa Photo of the Day</StyledH1>
+        <StyledH2>Welcome!</StyledH2>
+        <StyledP>Enjoy it! Learn it!</StyledP>
+      </FlexContainer>
       <DailyPhoto photoData={photoData} />
-    </div>
+    </StyledAppDiv>
   );
 }
 
